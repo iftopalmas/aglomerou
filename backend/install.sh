0cba25a7a1b65cc70d080a79ab7546c823f8007e
Manoel Campos <manoelcampos@gmail.com>
2020-05-02 12:41:07 -0300

    Update #2

--- backend/install.sh
+++ backend/install.sh
@@ -2,13 +2,12 @@
 
 # https://github.com/nvm-sh/nvm
 
-echo "Instalando ferramentas de desenvolvimento"
-
-VERSAO_NODE="14.1.0"
+echo ""; echo "# INSTALANDO FERRAMENTAS DE DESENVOLVIMENTO"; echo ""
 
 clear
 
 # Instala o nvm
+echo ""; echo "## INSTALANDO NVM"; echo ""
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # Carrega o nvm
@@ -16,8 +15,13 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Instala o Node.js
-nvm install $VERSAO_NODE
+echo ""; echo "## INSTALANDO VERSÃO DO NODE DEFINIDA EM .nvmrc"; echo ""
+nvm install `cat .nvmrc`
 
 # Coloca em uso a versão instalada do node
+echo ""; echo "## USANDO VERSÃO DO NODE DEFINIDA EM .nvmrc"; echo ""
 nvm use
+
+source ~/.bashrc
+echo ""; echo "## REABRA O TERMINAL SE NÃO CONSEGUIR USAR nvm, node ou npm"; echo ""
 
