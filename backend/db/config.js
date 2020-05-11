f7db5f4e26690a238f6010ffd262848db70d8f26
silvioantonio <oliveira_0000@hotmail.com>
2020-05-11 16:43:40 -0300

    Removido comentarios

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,15 +1,5 @@
-require('dotenv').config()
+
 const pool = require('pg');
-
-/*
-const connectionString = 
-`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-
-const pool = new Pool({ connectionString: connectionString })
-console.log(pool)
-*/
-
 
 const client = new pool.Client({
     user: process.env.DB_USER,
