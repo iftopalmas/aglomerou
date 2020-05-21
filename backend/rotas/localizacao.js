3c8cdda426586019529e92a0aa4f8ef51d9cbf77
Manoel Campos <manoelcampos@gmail.com>
2020-05-21 18:30:17 -0300

    Documenta com swagger a rota de inserir localizacao do dispositivo.

--- backend/rotas/localizacao.js
+++ backend/rotas/localizacao.js
@@ -18,6 +18,31 @@ const localizacaoController = require('../controllers/localizacao.controller');
  */
 localizacao.get('/:idDispositivo', localizacaoController.getUltimaLocalizacao);
 
+/**
+ * @swagger
+ * /localizacao/{idDispositivo}/{lat}/{long}:
+ *  post: 
+ *    parameters:
+ *      - name: idDispositivo
+ *        in: path
+ *        description: id do dispositivo
+ *        required: true
+ *        type: integer
+ *      - name: lat
+ *        in: path
+ *        description: latitude da localização do dispositivo
+ *        required: true
+ *        type: integer
+ *      - name: long
+ *        in: path
+ *        description: longitude da localização do dispositivo
+ *        required: true
+ *        type: integer
+ *    description: Insere a localização de um dispositivo com um detemrinado id
+ *    responses:
+ *      '200': 
+ *        description: Localização do dispositivo inserida com sucesso 
+ */
 localizacao.post('/:idDispositivo/:lat/:long', localizacaoController.inserir);
 
 module.exports = localizacao;
