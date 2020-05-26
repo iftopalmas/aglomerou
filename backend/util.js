2697617620d096de36d51ec4623b519a4e58d788
Manoel Campos <manoelcampos@gmail.com>
2020-05-26 15:37:16 -0300

    Close #54

--- backend/util.js
+++ backend/util.js
@@ -5,3 +5,15 @@
  * @param max valor máximo (inclusive)
  */
 exports.inRange = (number, min, max) => ( number - min ) * ( number - max ) <= 0;
+
+/**
+ * Retorna um HTTP status code 500 para exceções
+ * desconhecidas capturadas em um bloco catch.
+ * @param response Objeto Response para devolver uma resposta à requisição HTTP
+ * @param error Objeto contendo informações sobre o erro capturado, 
+ *              que será exibido no console.
+ */
+exports.serverError = (response, error) => {
+  response.status(500).send({message: "Erro interno do servidor"});
+  console.error(error.message, error.stack);
+};
