ae8e2b79ba66f53f4b91b97c3a26b8d7b5683cd1
Manoel Campos <manoelcampos@gmail.com>
2020-05-26 17:05:27 -0300

    Renomeia pasta backend/rotas para backend/routes para conformidade, pois todas as outras pastas estão com nome em inglês.

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-const rotas = require('./rotas');
+const rotas = require('./routes');
 app.use('/', rotas);
 
 require('./config/swagger')(app);
