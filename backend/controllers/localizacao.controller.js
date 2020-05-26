ed303e23fa36dba1e41b6c6a3d9dae1214b2cada
silvioantonio <oliveira_0000@hotmail.com>
2020-05-26 11:21:20 -0300

    Close #44 - Validar latitude e longitude

--- backend/controllers/localizacao.controller.js
+++ backend/controllers/localizacao.controller.js
@@ -27,17 +27,24 @@ exports.inserir = async (req, res) => {
     const long = req.params.long;
 
     try {
-         const result = await db.query(
-             " INSERT INTO localizacao_dispositivo ( id_dispositivo, latitude, longitude) " +
-             " VALUES ( $1, $2, $3 )",
-             [idDispositivo, lat, long]
-         );
+        if( inRange( lat, -90, 90 ) ) {
+            if(inRange( long, -180, 180 ) ) {
+                const result = await db.query(
+                    " INSERT INTO localizacao_dispositivo ( id_dispositivo, latitude, longitude) " +
+                    " VALUES ( $1, $2, $3 )",
+                    [idDispositivo, lat, long]
+                );
+                res.status(201).send({message: "Local inserido com Sucesso!"});
+            } else
+                res.status(404).send({message: "Valor de Longitude Incorreto!"});
+        }else
+            res.status(404).send({message: "Valor de Latitude Incorreto!"});
          
-         res.status(201).send({message: "Local inserido com Sucesso!"});
      } catch (error) {
          res.status(400).send({message: "Falha ao inserir localização"});
          console.error('connection error', error.message, error.stack);
      }
 };
 
+function inRange(number, min, max) { return ( ( number - min ) * ( number - max ) <= 0 ); }
 
