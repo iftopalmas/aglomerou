36cf7a0ea965887611d842b61eb38de1690d5a16
Manoel Campos <manoelcampos@gmail.com>
2020-06-01 15:39:41 -0300

    - Adiciona rotas para obter dispositivo, total de dispositivos e registrar dispositivo. - Altera parâmetro ID por UID. Como este último é um valor gerado pelo app mobile,   ele é obtido automaticamente pelo app. O ID gerado pelo banco não será gravado no app.

--- backend/routes/dispositivo.js
+++ backend/routes/dispositivo.js
@@ -1,25 +1,58 @@
 const router = require('express').Router();
-
-const getDispositivo = async(req, res) => {
-    //TODO: buscar dados do BD
-    res.status(200).json({ id: req.params.id, ativo: true });
-};
+const controller = require('../controllers/dispositivo.controller');
 
 /**
  * @swagger
- * /dispositivo/{id}:
+ * /dispositivo/total:
+ *  get: 
+ *    description: Obtém o total de dispositivos cadastrados
+ *    responses:
+ *      '200': 
+ *        description: Total de dispositivos obtido com sucesso 
+ */
+router.get('/total', controller.total);
+
+/**
+ * @swagger
+ * /dispositivo/{uid}:
  *  get: 
  *    parameters:
- *      - name: id
+ *      - name: uid
  *        in: path
- *        description: id do dispositivo
+ *        description: Identificador único do dispositivo
  *        required: true
- *        type: integer
- *    description: Obtém um dispositivo pelo id
+ *        type: string
+ *    description: Obtém um dispositivo pelo uid
  *    responses:
  *      '200': 
  *        description: Dispositivo obtido com sucesso 
  */
-router.get('/:id', getDispositivo);
+router.get('/:uid', controller.get);
+
+/**
+ * @swagger
+ * /dispositivo/{uid}/{tipo}:
+ *  post: 
+ *    parameters:
+ *      - name: uid
+ *        in: path
+ *        description: id único do dispositivo
+ *        required: true
+ *        type: string
+ *      - name: tipo
+ *        in: path
+ *        description: Tipo do dispositivo
+ *        required: true
+ *        type: string
+ *    description: Registra um dispositivo com um determinado UID (Unique Identifier)
+ *    responses:
+ *      '201': 
+ *        description: Dispositivo registrado com sucesso
+ *      '409': 
+ *        description: Dispositivo com o UID informado já foi registrado
+ *      '422': 
+ *        description: Tipo de dispositivo desconhecido
+ */
+router.post('/:uid/:tipo', controller.inserir);
 
 module.exports = router;
