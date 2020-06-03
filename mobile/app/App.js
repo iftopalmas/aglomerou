c292774badada9503797e32a256b2b9bf892926e
Ragir Fernando <ragirfernando@gmail.com>
2020-06-03 16:54:24 -0300

    Close #16 (PR #85)

--- mobile/app/App.js
+++ mobile/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import IdDispositivo from './componentes/IdDispositivo'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <IdDispositivo />
     </View>
   );
 }
---
+++ mobile/app/componentes/IdDispositivo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import api from '../service/api';
+
+
+export default class IdDispositivo extends Component {
+
+    componentDidMount() {
+        this.dispositivos();
+    }
+
+    dispositivos = async () => {
+        const idDispositivo = Constants.installationId;
+        const tipoDispositivo = Constants.deviceName;
+        try {
+            const response = await api.post('/dispositivo/' + idDispositivo + '/' + tipoDispositivo + '');
+        } catch (error) {
+            alert('Erro ' + error);
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Projeto Aglomerou</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
