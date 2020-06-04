6b1b094e47f7611fecd14ac6479763949fb8ccc2
silvioantonio <oliveira_0000@hotmail.com>
2020-06-04 15:06:47 -0300

    Close #88 - Configuração do pool de conexões

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,20 @@ const client = new pool.Client({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    max: 20,
+    idleTimeoutMillis: 15000,
+    connectionTimeoutMillis: 5000,
 });
 
-client.connect(() => {
+client.connect( err => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack);
+    }
     console.log('Base de dados conectado com sucesso!');
 });
+
+client.end( () => { console.log('O cliente desconectou!'); });
+
 
 module.exports = {
     query: (text, params) => client.query(text, params)
