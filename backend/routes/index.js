35180303e756e8458718d7f5158bc7f105c35987
silvioantonio <oliveira_0000@hotmail.com>
2020-06-05 14:35:20 -0300

    Close #89 - Rotas para gerenciamento de usuarios

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const rotas = require('express').Router();
 const dispositivo = require('./dispositivo');
 const localizacao = require('./localizacao');
+const usuario = require('./usuario');
 
 
 rotas.get('/', (req, res) => res.sendFile('/index.html'));
 
 rotas.use('/dispositivo', dispositivo);
 rotas.use('/localizacao', localizacao);
+rotas.use('/usuario', usuario);
 
 module.exports = rotas;
---
+++ backend/routes/usuario.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const controller = require('../controllers/usuario.controller');
+
+router.post('',controller.inserir);
+
+router.put('',controller.atualizar);
+
+router.delete('/:id', controller.deletar);
+
+module.exports = router;
