5d35dc580a54b387b6cabaa19c5bb922a00b770b
Manoel Campos <manoelcampos@gmail.com>
2020-06-07 16:57:49 -0300

    Atualiza script docker.sh para permitir alterar a porta externa do container do postgres. Mas a porta interna é sempre 5432.
    Carrega as variáveis de ambiente do .env.production
    para os dois containers, pois estava sendo usado apenas para
    o database.
    
    Atualiza tal arquivo para usar as configurações reais
    após configuração do domínio aglomerou.ifto.edu.br

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -19,7 +19,22 @@ if [[ $# -lt 2 ]]; then
 	exit -1
 fi
 
+env_vars()
+{
+	# As variáveis do ambiente de produção são usadas
+	# apenas quando for rodar o container.
+	# Assim, nenhuma informação possivelmente sensível
+	# é armazenada dentro do imagem públic em hub.docker.com
+	if [[ ! -f .env.production ]]; then
+		echo "Arquivo .env.production não foi localizado. Copie a partir de backend/.env" >&2
+		exit -1
+	fi
+
+	source .env.production
+}
+
 if [[ $1 == "backend" ]]; then
+	env_vars
 	IMAGE_NAME="manoelcampos/aglomerou:backend"
 	CONTAINER_NAME="aglomerou-backend"
 	#Usa porta 8080 pra não ter que rodar o node como root no container
@@ -32,19 +47,10 @@ if [[ $1 == "backend" ]]; then
 		docker build -f ../backend/Dockerfile -t $IMAGE_NAME ../backend
 	elif [[ $2 == "run" ]]; then
 		# Executar o container em background (-d)
-		docker run --name $CONTAINER_NAME -d -p 80:$PORT -e PORT=$PORT $IMAGE_NAME
+		docker run --name $CONTAINER_NAME -d -p 80:$PORT --env-file .env.production $IMAGE_NAME
 	fi
 elif [[ $1 == "database" ]]; then
-	# As variáveis do ambiente de produção são usadas
-	# apenas quando for rodar o container.
-	# Assim, nenhuma informação possivelmente sensível
-	# é armazenada dentro do imagem públic em hub.docker.com
-	if [[ ! -f .env.production ]]; then
-		echo "Arquivo .env.production não foi localizado. Copie a partir de backend/.env" >&2
-		exit -1
-	fi
-
-	source .env.production
+	env_vars
 	IMAGE_NAME="manoelcampos/aglomerou:database"
 	CONTAINER_NAME="aglomerou-postgres"
 
@@ -56,11 +62,11 @@ elif [[ $1 == "database" ]]; then
 		# Executar o container em background (-d)
 		docker run -d --name $CONTAINER_NAME \
 				-e POSTGRES_USER=$DB_USER -e POSTGRES_PASSWORD=$DB_PASSWORD \
-				-p $DB_PORT:$DB_PORT $IMAGE_NAME
-		echo "Use $0 connect pra conectar ao servidor Postgres no container criado" >&2
+				-p $DB_PORT:5432 $IMAGE_NAME
+		echo "Use $0 database connect pra conectar ao servidor Postgres no container criado" >&2
 	elif [[ $2 == "connect" ]]; then
 		#https://www.postgresql.org/docs/9.1/libpq-envars.html
-		PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER $DB_DATABASE 
+		PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_DATABASE 
 	fi
 else
 	echo "Informe backend ou database para construir um desses containers." >&2
