bb402286cd621e927ace2baff292b7ebbe612bdb
Manoel Campos <manoelcampos@gmail.com>
2020-06-07 11:56:58 -0300

    Sai da função de inclusão de usuário se o email for inválido, senão a requisição continua sendo processada.

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -2,8 +2,14 @@ const db = require("../config/db");
 const { serverError } = require("../util");
 
 exports.inserir = async (req, res) => {
+    console.log("\n\noi");
     const { data_hora_cadastro, ativo, email, senha } = req.body;
-    if (!validateEmail(email)) { res.status(400).send({message: "Um valor valido deve ser passado como EMAIL!"}); }
+    if (!validateEmail(email)) { 
+        res.status(400).send({message: "Um valor valido deve ser passado como EMAIL!"}); 
+        return;
+    }
+
+    
     try {
         await db.query(
             "INSERT INTO usuario ( data_hora_cadastro, ativo, email, senha ) VALUES ( $1, $2, $3, $4 )",
