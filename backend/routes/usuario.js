b8bec4e91190bc59f131c5c071767754b5abd59d
Manoel Campos <manoelcampos@gmail.com>
2020-06-07 15:21:40 -0300

    Close #114

--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -1,10 +1,63 @@
 const router = require('express').Router();
 const controller = require('../controllers/usuario.controller');
 
+/**
+ * @swagger
+ * /usuario:
+ *  post: 
+ *    parameters:
+ *      - name: usuario
+ *        in: body
+ *        description: Usuário a ser inserido
+ *        required: true
+ *        type: object
+ *    description: Cadastra um usuário para acesso à aplicação web
+ *    responses:
+ *      '201': 
+ *        description: Usuário cadastrado com sucesso
+ *      '409': 
+ *        description: Usuário com o email informado já foi cadastrado
+ */
 router.post('/', controller.inserir);
 
+/**
+ * @swagger
+ * /usuario:
+ *  put: 
+ *    parameters:
+ *      - name: usuario
+ *        in: body
+ *        description: Usuário a ser atualizado
+ *        required: true
+ *        type: object
+ *    description: Atualiza um usuário da aplicação web
+ *    responses:
+ *      '204': 
+ *        description: Usuário atualizado com sucesso
+ *      '404': 
+ *        description: Usuário não encontrado
+ *      '409': 
+ *        description: Usuário com o email informado já foi cadastrado
+ */
 router.put('/', controller.atualizar);
 
+/**
+ * @swagger
+ * /usuario:
+ *  delete: 
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: Id do Usuário a ser excluído
+ *        required: true
+ *        type: integer
+ *    description: Atualiza um usuário da aplicação web
+ *    responses:
+ *      '200': 
+ *        description: Usuário excluído com sucesso
+ *      '404': 
+ *        description: Usuário não encontrado
+ */
 router.delete('/:id', controller.deletar);
 
 module.exports = router;
