7eab573f5e796e0b8026c7b0c4e05b10f4633212
Manoel Campos <manoelcampos@gmail.com>
2020-06-15 11:34:09 -0300

    Remove mensagem padrão da tela inicial do app

--- mobile/app/App.js
+++ mobile/app/App.js
@@ -5,7 +5,6 @@ import LocalizacaoDispositivo from './componentes/LocalizacaoDispositivo'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <IdDispositivo />
       <LocalizacaoDispositivo/>
     </View>
