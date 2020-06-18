83e0ca924f528272cdc833247f531655f45c36a3
Thányla Sales <thanylas@gmail.com>
2020-06-18 18:20:28 -0300

    Close #15

--- mobile/app/App.js
+++ mobile/app/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import IdDispositivo from './componentes/IdDispositivo'
 import LocalizacaoDispositivo from './componentes/LocalizacaoDispositivo'
+import MapView from 'react-native-maps';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <IdDispositivo />
       <LocalizacaoDispositivo/>
+
+      <MapView style={styles.mapStyle} />
     </View>
   );
 }
@@ -18,4 +22,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
