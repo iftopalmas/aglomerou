30f496048e0517d600d0d6e19da78a345d61721f
Manoel Campos <manoelcampos@gmail.com>
2020-06-30 19:06:43 -0300

    Usa template string para definir a baseURL do axios no mobile. Sem isso, o número da porta estava sendo removido do endereço. O número de porta é usado no ambiente de desenvolvimento, como 8080 para o backend.

--- mobile/app/service/api.js
+++ mobile/app/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Constants from 'expo-constants'
 
 const api = axios.create({
-    baseURL: Constants.manifest.extra.addressApiAglomerou
+    baseURL: `${Constants.manifest.extra.addressApiAglomerou}`
 });
 
 export default api;
