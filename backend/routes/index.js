1131899c49ce71379a8ff4786b77c7336b80d5d9
Manoel Campos <manoelcampos@gmail.com>
2020-07-01 11:49:10 -0300

    Renomeia index.html para default.html no backend, para não apresentar tal página por padrão e apenas direcionar para o GitHub Pages.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,15 @@ const dispositivo = require('./dispositivo');
 const localizacao = require('./localizacao');
 const usuario = require('./usuario');
 
+//rotas.get('/', (req, res) => res.sendFile('/index.html'));
 
-rotas.get('/', (req, res) => res.sendFile('/index.html'));
+rotas.get('/', (req, res) => {
+    console.log('redirect');
+    
+    res.writeHead(302, {'Location': 'https://ifto-palmas.github.io/aglomerou/'});
+    res.end();
+});
+  
 
 rotas.use('/dispositivo', dispositivo);
 rotas.use('/localizacao', localizacao);
