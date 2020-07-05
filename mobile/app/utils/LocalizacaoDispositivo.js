51eda972e8d9eacf1be419a6d67f9f20a9ab82ca
Ana Beatriz <annabhiaaraujo.silva@gmail.com>
2020-07-05 18:21:18 -0300

    Adicionando TaskManager para rastreamento de localização em background.

--- mobile/app/utils/LocalizacaoDispositivo.js
+++ mobile/app/utils/LocalizacaoDispositivo.js
@@ -1,33 +1,46 @@
 import * as Location from 'expo-location';
 import Constants from 'expo-constants';
+import * as TaskManager from 'expo-task-manager';
 
+const LOCATION_TASK_NAME = 'background-location-task';
 const uid = Constants.installationId;
 import api from '../service/api';
 
-const getLocalizacaoDispositivo = async () => {      
+const getLocalizacaoDispositivo = async () => {
   try {
-      const { status } = await Location.requestPermissionsAsync();
-      if (status !== 'granted') {
-          setErrorMsg('A permissão para acessar a localização do dispositivo foi negada!');
-          return;
-      }
+    const { status } = await Location.requestPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('A permissão para acessar a localização do dispositivo foi negada!');
+      return;
+    }
 
-      const location = await Location.getCurrentPositionAsync();
-      const {latitude, longitude} = location.coords
-      return {latitude, longitude}
+    const location = await Location.getCurrentPositionAsync();
+    const { latitude, longitude } = location.coords
+    return { latitude, longitude }
   } catch (error) {
-      console.log(`Erro ao obter localização: ${error}`);
+    console.log(`Erro ao obter localização: ${error}`);
   }
 };
 
-const enviarLocalizacaoParaServidor = async (latitude, longitude) => {      
+const enviarLocalizacaoParaServidor = async (latitude, longitude) => {
   try {
     const url = `/localizacao/${uid}/${latitude}/${longitude}`;
-    console.log(api.defaults.baseURL+url);
+    console.log(api.defaults.baseURL + url);
     const response = await api.post(url);
   } catch (error) {
     console.log(`Erro ao enviar localização: ${error}`);
   }
 }
 
-export {getLocalizacaoDispositivo, enviarLocalizacaoParaServidor};
+const enviarLocalizacaoBackground = async () => {
+  const { status } = await Location.requestPermissionsAsync();
+  if (status === 'granted') {
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      accuracy: Location.Accuracy.Balanced,
+    });
+  }
+}
+
+TaskManager.defineTask(LOCATION_TASK_NAME, enviarLocalizacaoBackground());
+
+export { getLocalizacaoDispositivo, enviarLocalizacaoParaServidor, enviarLocalizacaoBackground};
