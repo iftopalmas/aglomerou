042976c5778e929bc8fa2d9af71469649894fcb9
Lucca Radaeli <radaelidev@gmail.com>
2020-07-08 17:05:59 -0300

    Refatorando
    Refatora getLocaisRecentesDispositivos e adiciona condicional do render dos markers

--- mobile/app/componentes/Mapa.js
+++ mobile/app/componentes/Mapa.js
@@ -1,73 +1,91 @@
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { FontAwesome5 as Fa } from '@expo/vector-icons';
 import { Marker } from 'react-native-maps';
-import MapView from 'react-native-map-clustering'
+import MapView from 'react-native-map-clustering';
 
-import LocalizacaoDispositivo from './LocalizacaoDispositivo'
+import LocalizacaoDispositivo from './LocalizacaoDispositivo';
 
-import { getLocalizacaoDispositivo, getLocalizacoesRecentes } from '../utils/LocalizacaoDispositivo'
+import {
+  getLocalizacaoDispositivo,
+  getLocalizacoesRecentes,
+} from '../utils/LocalizacaoDispositivo';
 
 export default function App() {
-  const [localizacoes, setLocalizacoes] = useState([])
-  const [latitudeInicial, setLatitudeInicial] = useState()
-  const [longitudeInicial, setLongitudeInicial] = useState()
-  const [loading, setLoading] = useState(true)
-  
-  useEffect(() => {
-    const getLocalizaoInicial = async () => {          
-      try {
-          const {latitude, longitude} = await getLocalizacaoDispositivo()
-          setLatitudeInicial(latitude);
-          setLongitudeInicial(longitude);
-          setLoading(false);
-          
-          setLocalizacoes(await getLocalizacoesRecentes())
-      } catch (error) {
-          console.error(`Erro ao obter localização inicial: ${error}`)
-          // Define a localização inicial como Praça dos Girassóis.
-          setLatitudeInicial(-10.184510);
-          setLongitudeInicial(-48.334660);
-          setLoading(false)
-      }        
-    }
+  const [localizacoes, setLocalizacoes] = useState([]);
+  const [latitudeInicial, setLatitudeInicial] = useState();
+  const [longitudeInicial, setLongitudeInicial] = useState();
+  const [loading, setLoading] = useState(true);
 
-    getLocalizaoInicial()    
-  }, [])
+  useEffect(() => {
+    const getLocaisRecentesMarkers = async () => {
+      try {
+        const locaisMarkers = await getLocalizacoesRecentes();
+        setLocalizacoes(locaisMarkers);
+      } catch (error) {
+        console.log(
+          'Não foi possível buscar as localizações para os markers: ',
+          error
+        );
+      }
+    };
+    const getLocalizaoInicial = async () => {
+      try {
+        const { latitude, longitude } = await getLocalizacaoDispositivo();
+        setLatitudeInicial(latitude);
+        setLongitudeInicial(longitude);
+        setLoading(false);
+      } catch (error) {
+        console.error(`Erro ao obter localização inicial: ${error}`);
+        // Define a localização inicial como Praça dos Girassóis.
+        setLatitudeInicial(-10.18451);
+        setLongitudeInicial(-48.33466);
+        setLoading(false);
+      }
+    };
+
+    getLocalizaoInicial();
+    getLocaisRecentesMarkers();
+  }, []);
 
   return (
     <View style={styles.container}>
-      {loading ? <Text>Carregando Mapa...</Text> : (
-        <MapView 
-        style={styles.mapStyle} 
-        initialRegion={{
-          latitude: latitudeInicial,
-          longitude: longitudeInicial,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05
-        }}>
-          
-        <Marker
-          coordinate={{
+      {loading ? (
+        <Text>Carregando Mapa...</Text>
+      ) : (
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={{
             latitude: latitudeInicial,
             longitude: longitudeInicial,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
           }}
         >
-          <Fa name="map-marker-alt" size={32} color="#e02041" />         
-        </Marker>
-
-        {localizacoes.map((item, index) => (
-           <Marker key={index}
+          <Marker
             coordinate={{
-              latitude: parseFloat(item.latitude),
-              longitude: parseFloat(item.longitude),
+              latitude: latitudeInicial,
+              longitude: longitudeInicial,
             }}
-           /> 
-          ))}
-      </MapView>
+          >
+            <Fa name="map-marker-alt" size={32} color="#e02041" />
+          </Marker>
+          {localizacoes.length > 0 ? (
+            localizacoes.map((local) => (
+              <Marker
+                key={local + Math.random()}
+                coordinate={{
+                  latitude: parseFloat(local.latitude),
+                  longitude: parseFloat(local.longitude),
+                }}
+              />
+            ))
+          ) : (
+            <View />
+          )}
+        </MapView>
       )}
-    <LocalizacaoDispositivo/>
+      <LocalizacaoDispositivo />
     </View>
   );
 }
@@ -83,5 +101,5 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-  },  
+  },
 });
