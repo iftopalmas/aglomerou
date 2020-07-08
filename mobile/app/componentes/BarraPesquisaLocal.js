1e003efc89eca5030b3f7c5cffec4552b9b9687c
Lucca Radaeli <radaelidev@gmail.com>
2020-07-08 19:15:22 -0300

    Close #155 (PR #179)
    * Adicionando barra de pesquisa por local.
    * Atualiza markers (locais dispositivos) quando usuário busca por um local.

---
+++ mobile/app/componentes/BarraPesquisaLocal.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { REACT_NATIVE_GOOGLE_MAPS_API_KEY } from 'react-native-dotenv';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+export default function BarraPesquisa({ moverMapa }) {
+  const [searchFocused, setSearchfocused] = useState(false);
+
+  return (
+    <GooglePlacesAutocomplete
+      placeholder="Pesquise aqui"
+      placeholderTextColor="#326002"
+      onPress={(data, details) => {
+        moverMapa(details.geometry.location);
+      }}
+      query={{
+        key: REACT_NATIVE_GOOGLE_MAPS_API_KEY,
+        language: 'pt-BR',
+        components: 'country:br',
+      }}
+      textInputProps={{
+        onFocus: () => {
+          setSearchfocused(true);
+        },
+        onBlur: () => {
+          setSearchfocused(false);
+        },
+        autoCapitalize: 'none',
+        autoCorrect: false,
+      }}
+      listViewDisplayed={searchFocused}
+      fetchDetails
+      enablePoweredByContainer={false}
+      styles={styles}
+    />
+  );
+}
+
+const styles = {
+  container: {
+    position: 'absolute',
+    top: 40,
+    alignItems: 'center',
+    width: '96%',
+  },
+  textInputContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    height: 56,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    width: '100%',
+  },
+  textInput: {
+    height: 48,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 8,
+
+    fontSize: 16,
+    color: '#41414d',
+  },
+  listView: {
+    backgroundColor: '#fff',
+    marginTop: 8,
+    borderRadius: 8,
+
+    width: '96%',
+  },
+  description: {
+    fontSize: 14,
+  },
+  row: {
+    paddingHorizontal: 8,
+    height: 48,
+    marginBottom: 4,
+    alignItems: 'center',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#326002',
+    opacity: 0.1,
+  },
+};
--- mobile/app/componentes/Mapa.js
+++ mobile/app/componentes/Mapa.js
@@ -1,73 +1,136 @@
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { FontAwesome5 as Fa } from '@expo/vector-icons';
 import { Marker } from 'react-native-maps';
-import MapView from 'react-native-map-clustering'
+import MapView from 'react-native-map-clustering';
 
-import LocalizacaoDispositivo from './LocalizacaoDispositivo'
+import LocalizacaoDispositivo from './LocalizacaoDispositivo';
+import BarraPesquisa from './BarraPesquisaLocal';
 
-import { getLocalizacaoDispositivo, getLocalizacoesRecentes } from '../utils/LocalizacaoDispositivo'
+import {
+  getLocalizacaoDispositivo,
+  getLocalizacoesRecentes,
+} from '../utils/LocalizacaoDispositivo';
 
 export default function App() {
-  const [localizacoes, setLocalizacoes] = useState([])
-  const [latitudeInicial, setLatitudeInicial] = useState()
-  const [longitudeInicial, setLongitudeInicial] = useState()
-  const [loading, setLoading] = useState(true)
-  
-  useEffect(() => {
-    const getLocalizaoInicial = async () => {          
-      try {
-          const {latitude, longitude} = await getLocalizacaoDispositivo()
-          setLatitudeInicial(latitude);
-          setLongitudeInicial(longitude);
-          setLoading(false);
-          
-          setLocalizacoes(await getLocalizacoesRecentes())
-      } catch (error) {
-          console.error(`Erro ao obter localização inicial: ${error}`)
-          // Define a localização inicial como Praça dos Girassóis.
-          setLatitudeInicial(-10.184510);
-          setLongitudeInicial(-48.334660);
-          setLoading(false)
-      }        
-    }
+  const [localizacoes, setLocalizacoes] = useState([]);
+  const [latitudeInicial, setLatitudeInicial] = useState();
+  const [longitudeInicial, setLongitudeInicial] = useState();
+  const [localBuscado, setLocalBuscado] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [loading, setLoading] = useState(true);
 
-    getLocalizaoInicial()    
-  }, [])
+  const mapRef = useRef();
+
+  const moverMapa = (novoLocal) => {
+    console.log('movendo para o endereco recebido ->', novoLocal);
+
+    const region = {
+      latitude: novoLocal.lat,
+      longitude: novoLocal.lng,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009,
+    };
+
+    setLocalBuscado({
+      latitude: novoLocal.lat,
+      longitude: novoLocal.lng,
+    });
+
+    mapRef.current.animateToRegion(region, 1200);
+  };
+
+  // atualiza os markers no mapa
+  const getLocaisRecentesMarkers = async () => {
+    try {
+      const locaisMarkers = await getLocalizacoesRecentes();
+      setLocalizacoes(locaisMarkers);
+    } catch (error) {
+      console.log(
+        'Não foi possível buscar as localizações para os markers: ',
+        error
+      );
+    }
+  };
+
+  // monitora buscas por local e atualiza markers no mapa
+  useEffect(() => {
+    const atualizarMarkers = async () => {
+      await getLocaisRecentesMarkers();
+    };
+
+    console.log('Atualiando markers. Novo local ->', localBuscado);
+    atualizarMarkers();
+  }, [localBuscado]);
+
+  useEffect(() => {
+    const getLocalizaoInicial = async () => {
+      try {
+        const { latitude, longitude } = await getLocalizacaoDispositivo();
+        setLatitudeInicial(latitude);
+        setLongitudeInicial(longitude);
+
+        setLoading(false);
+      } catch (error) {
+        console.error(`Erro ao obter localização inicial: ${error}`);
+
+        // Define a localização inicial como Praça dos Girassóis.
+        setLatitudeInicial(-10.18451);
+        setLongitudeInicial(-48.33466);
+        setLoading(false);
+      }
+    };
+
+    getLocalizaoInicial();
+    getLocaisRecentesMarkers();
+  }, []);
 
   return (
     <View style={styles.container}>
-      {loading ? <Text>Carregando Mapa...</Text> : (
-        <MapView 
-        style={styles.mapStyle} 
-        initialRegion={{
-          latitude: latitudeInicial,
-          longitude: longitudeInicial,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05
-        }}>
-          
-        <Marker
-          coordinate={{
-            latitude: latitudeInicial,
-            longitude: longitudeInicial,
-          }}
-        >
-          <Fa name="map-marker-alt" size={32} color="#e02041" />         
-        </Marker>
-
-        {localizacoes.map((item, index) => (
-           <Marker key={index}
-            coordinate={{
-              latitude: parseFloat(item.latitude),
-              longitude: parseFloat(item.longitude),
+      {loading ? (
+        <Text>Carregando Mapa...</Text>
+      ) : (
+        <>
+          <MapView
+            ref={mapRef}
+            style={styles.mapStyle}
+            initialRegion={{
+              latitude: latitudeInicial,
+              longitude: longitudeInicial,
+              latitudeDelta: 0.05,
+              longitudeDelta: 0.05,
             }}
-           /> 
-          ))}
-      </MapView>
+          >
+            <Marker
+              coordinate={{
+                latitude: latitudeInicial,
+                longitude: longitudeInicial,
+              }}
+            >
+              <Fa name="map-marker-alt" size={32} color="#e02041" />
+            </Marker>
+            {localizacoes.length > 0 ? (
+              localizacoes.map((local) => (
+                <Marker
+                  key={local + Math.random()}
+                  coordinate={{
+                    latitude: parseFloat(local.latitude),
+                    longitude: parseFloat(local.longitude),
+                  }}
+                >
+                  <Fa name="map-marker-alt" size={32} color="#e02041" />
+                </Marker>
+              ))
+            ) : (
+              <View />
+            )}
+          </MapView>
+          <BarraPesquisa moverMapa={moverMapa} />
+        </>
       )}
-    <LocalizacaoDispositivo/>
+      <LocalizacaoDispositivo />
     </View>
   );
 }
@@ -83,5 +146,5 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-  },  
+  },
 });
