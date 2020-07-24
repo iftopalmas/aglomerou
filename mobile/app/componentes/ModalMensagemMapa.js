15abe61d5401bd7ee3c685171e79e6df316abc29
Ana Beatriz <annabhiaaraujo.silva@gmail.com>
2020-07-24 19:01:36 -0300

    Modal estilizado e botão OK adicionado.

--- mobile/app/componentes/ModalMensagemMapa.js
+++ mobile/app/componentes/ModalMensagemMapa.js
@@ -21,17 +21,21 @@ const ModalMensagemMapa = () => {
         }}
       >
         <View style={styles.centeredView}>
-          <TouchableHighlight
-            style={{ ...styles.openButton, backgroundColor: '#94D451' }}
-            onPress={() => {
-              setModalVisible(!modalVisible);
-            }}
-          >
-            <Text style={styles.textStyle}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>
               Para encontrar mais pontos de aglomeração, navegue pelo mapa ou
               pesquise o nome de um local no campo acima.
             </Text>
-          </TouchableHighlight>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: '#94D451' }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>OK</Text>
+            </TouchableHighlight>
+          </View>
         </View>
       </Modal>
     </View>
@@ -43,26 +47,50 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 22,
+    /* flex: 1,
+    backgroundColor: '#fffafa',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 10,
     marginBottom: 10,
-    width: 350,
-    padding: 30,
+    width: 300,
+    height: 5,
+    padding: 30, */
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 15,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 10,
   },
   openButton: {
     backgroundColor: '#94D451',
     borderRadius: 20,
-    padding: 10,
-    elevation: 2,
+    padding: 15,
+    elevation: 5,
     margin: 10,
+    width: 100,
   },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
+    fontSize: 15,
   },
   modalText: {
-    marginBottom: 15,
+    marginBottom: 5,
     textAlign: 'center',
+    color: '#41414d',
   },
 });
 
