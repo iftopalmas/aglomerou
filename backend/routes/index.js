a06abe171e0b59ebdc7148eb4c18f04b303d605f
silvioantonio <oliveira_0000@hotmail.com>
2020-07-25 15:07:51 -0300

    Close #207

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const rotas = require('express').Router();
 const dispositivo = require('./dispositivo');
 const localizacao = require('./localizacao');
+const notificacao = require('./notificacao');
 const usuario = require('./usuario');
   
 rotas.use('/dispositivo', dispositivo);
+rotas.use('/notificacao', notificacao);
 rotas.use('/localizacao', localizacao);
 rotas.use('/usuario', usuario);
 
