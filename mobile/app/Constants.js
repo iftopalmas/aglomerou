05e77f2d11635176a0f0caae3d620570c45a9431
Lucca Radaeli <radaelidev@gmail.com>
2020-07-27 14:26:13 -0300

    Criando lógica modal mensagem
    Mostra o modal uma vez e ao fechar defie no local storage uma chave de modal visto.

--- mobile/app/Constants.js
+++ mobile/app/Constants.js
@@ -4,5 +4,6 @@ const LOCATION_TASK_NAME = 'background-location-task';
  * Nome da chave que armazena o código do captcha em um arquivo local.
  */
 const CAPTCHA_STORAGE_ITEM = 'captcha';
+const MODAL_MAP_MESSAGE_ITEM = 'modalMapMessage';
 
-export { LOCATION_TASK_NAME, CAPTCHA_STORAGE_ITEM };
+export { LOCATION_TASK_NAME, CAPTCHA_STORAGE_ITEM, MODAL_MAP_MESSAGE_ITEM };
