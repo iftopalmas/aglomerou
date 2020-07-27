42c23495ba1190c8b0c9c58ac8c3ae66f14a9ed6
Lucca Radaeli <radaelidev@gmail.com>
2020-07-27 14:33:23 -0300

    resolve conflicts

--- mobile/app/componentes/ModalMensagemMapa.js
+++ mobile/app/componentes/ModalMensagemMapa.js
@@ -28,7 +28,11 @@ const ModalMensagemMapa = ({ modalVisible, fecharModal }) => {
             <TouchableHighlight
               style={{ ...styles.openButton, backgroundColor: '#94D451' }}
               onPress={() => {
+<<<<<<< HEAD
                 fecharModal();
+=======
+                setModalVisible(!modalVisible);
+>>>>>>> Atualiza o model de informações sobre uso do mapa (#213)
               }}
             >
               <Text style={styles.textStyle}>OK</Text>
