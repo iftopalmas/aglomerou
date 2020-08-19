a4b5b9ef4beb98c55c2e2b1d2394548d3a06b075
Manoel Campos <manoelcampos@gmail.com>
2020-08-19 14:11:15 -0300

    Close #232

--- backend/routes/notificacao.js
+++ backend/routes/notificacao.js
@@ -37,4 +37,17 @@ const controller = require("../controllers/notificacao.controller");
  */
 router.post('/', controller.inserir);
 
+/**
+ * @swagger
+ * /api/notificacao:
+ *  get: 
+ *    summary: Obtém as notificações, com valores médios para o total de pessoas por local, nos últimos 5 minutos.
+ *    responses:
+ *      '200': 
+ *        description: Notificações recentes obtidas com sucesso.
+ *        examples:
+ *          application/json: [{ "latitude": 90, "longitude": 180, estimativa_media_pessoas: 54 }, { "latitude": 92, "longitude": 150, estimativa_media_pessoas: 42 }]
+ */
+router.get('/', controller.getNotificacoesRecentes);
+
 module.exports = router;
