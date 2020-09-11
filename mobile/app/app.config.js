ad12a69632b437322a2a81c198a243234cbefc1c
Manoel Campos <manoelcampos@gmail.com>
2020-09-11 16:08:43 -0300

    Altera nome do pacote do app após rejeição na Play Store

--- mobile/app/app.config.js
+++ mobile/app/app.config.js
@@ -20,7 +20,7 @@ export default {
   assetBundlePatterns: ['**/*'],
   ios: {
     icon: './assets/ios-icon.png',
-    bundleIdentifier: 'br.edu.ifto.aglomerouapp',
+    bundleIdentifier: 'br.edu.ifto.apps.aglomerou',
     supportsTablet: true,
     config: {
       googleMapsApiKey: REACT_NATIVE_GOOGLE_MAPS_API_KEY,
@@ -28,7 +28,7 @@ export default {
   },
   android: {
     icon: './assets/android-icon.png',
-    package: 'br.edu.ifto.aglomerouapp',
+    package: 'br.edu.ifto.apps.aglomerou',
     config: {
       googleMaps: {
         apiKey: REACT_NATIVE_GOOGLE_MAPS_API_KEY,
